27d2a1b6c2ee1523d75b67dd013c9a310592af0a
orangeeeee <orangeeeee.ore.ore@gmail.com>
2018-12-08 18:56:26 +0900

    関数化

--- ImportEntityDictionary.py
+++ ImportEntityDictionary.py
@@ -1,5 +1,6 @@
 import os
 import csv
+
 # utf8で読み込む
 # col1 = TABLE_CATALOG, col2 = TABLE_SCHEMA, col3 = TABLE_NAME, col4 = COLUMN_NAME, col4= LOGICAL_NAME
 FILE_PATH = os.path.join('file', 'import', '')
@@ -7,17 +8,22 @@ IMPORT_FILE_NAME = 'a5m2_COLUMNS'
 COLUMN_ENTITY_NAME = 'COLUMN_NAME'
 COLUMN_ENTITY_JP_NAME = 'LOGICAL_NAME'
 
-entity_dictionary_file = open(FILE_PATH + IMPORT_FILE_NAME + '.csv', encoding="utf_8")
-csv_file_obj = csv.DictReader(entity_dictionary_file)
+"""
+A5ERのCSV Export機能を使用し、出力したCSVを読込み
+key:論理名、value:物理名のdictionaryを作成する
+"""
+def importDictionary():
 
-# 辞書作成
-# TODO dictionaryに直す
-e_dictionary = {}
+    entity_dictionary_file = open(FILE_PATH + IMPORT_FILE_NAME + '.csv', encoding="utf_8")
+    csv_file_obj = csv.DictReader(entity_dictionary_file)
 
-for row in csv_file_obj:
-    key = row[COLUMN_ENTITY_JP_NAME]
-    value = row[COLUMN_ENTITY_NAME]
-    e_dictionary[key] = value
+    e_dictionary = {}
 
-entity_dictionary_file.close()
+    for row in csv_file_obj:
+        key = row[COLUMN_ENTITY_JP_NAME]
+        value = row[COLUMN_ENTITY_NAME]
+        e_dictionary[key] = value
 
+    entity_dictionary_file.close()
+
+    return e_dictionary
